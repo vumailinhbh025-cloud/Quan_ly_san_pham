192c466e99659cbfa200b11b3c0f0d4c05f2799d
Linh Vu <vumailinh.bh025@gmail.com>
2026-01-11 21:43:59 +0700

    'finish'

--- JS/quan_ly_san_pham.js
+++ JS/quan_ly_san_pham.js
@@ -1,37 +1,120 @@
-import { Product } from "../models/Product.js"
+import { Product } from "../models/Product.js" 
 
-let arrProduct = [];
-document.querySelector('#btnAddProduct').onclick = function () {
-    //input: product: object
+let arrProduct = []
+document.querySelector("#btnAddProduct").onclick = function (){
     let product = new Product();
-    let arrInput = document.querySelectorAll('#form_product .form-control, #form_product .form-select');
-    for (let input of arrInput) {
-        let { id, value, style } = input;
-        product[id] = value;
-        style.color = '#fff';
+    let arrInput = document.querySelectorAll("#form_product .form-control, #form_product .form-select")
+    for (let input of arrInput){
+        let {id, value} = input;
+        product[id] =value; 
     }
     arrProduct.push(product);
-    console.log(arrProduct);
-    //Gọi hàm từ mảng product tạo ra table product
+    console.log(arrProduct)
+    //Goi ham tao bang
     loadTableProduct(arrProduct);
+    //Luu du lieu vao localstorage
+    saveProducts();
 }
-                                                
-window.loadTableProduct = function (arrProd){ 
-    let htmlTr = '';
-    for(let prod of arrProd){
+
+window.loadTableProduct = function(arrProd){
+    let htmlTr = "";
+    for (let prod of arrProd){
         htmlTr += `
-            <tr>
-                <td>${prod.id}</td>
-                <td>${prod.name}</td>
-                <td>${prod.price}</td>
-                <td>${prod.img}</td>
-                <td>${prod.type}</td>
-                <td>
-                    <button class="btn btn-danger" onclick="deleteProduct('${prod.id}')">Delete</button>
-                    <button class="btn btn-primary mx-2" onclick="edit('${prod.id}')">Edit</button>
-                </td>
-            </tr>
-        `
+        <tr>
+            <td>${prod.id}</td>
+            <td>${prod.name}</td>
+            <td>${prod.price}</td>
+            <td>${prod.img}</td>
+            <td>${prod.type}</td>
+            <td>
+            <button class="btn btn-danger" onclick ="deleteProduct('${prod.id}')">Delete</button>
+            <button class="btn btn-primary" onclick ="edit('${prod.id}')">Edit</button>
+            </td>
+        </tr>`
     }
-    document.querySelector('#tblProduct').innerHTML = htmlTr;
+    document.querySelector("#tblProduct").innerHTML = htmlTr;
+}
+
+// Xoa san pham
+window.deleteProduct = function (idProductClick){
+    let indexDel = arrProduct.findIndex((prod) => prod.id == idProductClick);
+    if(indexDel != -1){
+        arrProduct.splice(indexDel,1)
+    }
+    loadTableProduct(arrProduct)
+}
+
+/** -------------------------------------------*/
+//Luu du lieu vao localstorage
+window.saveProducts = function(){
+    let sProduct= JSON.stringify(arrProduct);
+    localStorage.setItem("arrProduct", sProduct);
+}
+
+//Lay du lieu tu localstorage de hien thi len giao dien
+window.getProducts = function(){
+    if(localStorage.getItem("arrProduct")){
+        let sProduct = localStorage.getItem("arrProduct");
+        arrProduct = JSON.parse(sProduct); 
+        loadTableProduct(arrProduct)
+    }
+}
+
+//Goi ham khi trang duoc tai lai
+getProducts()
+
+/** -------------------------------------------*/
+
+//edit du lieu
+window.edit = function(idEdit){
+    let prodEdit = arrProduct.find((prod) => prod.id == idEdit);
+    if(prodEdit){
+        let arrInput = document.querySelectorAll("#form_product .form-control, #form_product .form-select");
+        for(let input of arrInput){
+            input.value = prodEdit[input.id]
+        }
+    }
+    document.querySelector("#id").disabled = true; 
+}
+
+// Cap nhat du lieu
+document.querySelector("#btnUpdate").onclick = ()=>{
+    let proUpdate = new Product(); 
+    let arrInput = document.querySelectorAll("#form_product .form-control, #form_product .form-select");
+    for (let input of arrInput){
+        let {id, value} = input;
+        proUpdate [id] = value;
+    }
+    // console.log ('prod', proUpdate)
+    let prodArray = arrProduct.find((item) => item.id == proUpdate.id ); 
+     if(prodArray){
+        // prodArray.id = prodUpdate.id;
+        prodArray.name = proUpdate.name;
+        prodArray.price = proUpdate.price;
+        prodArray.img = proUpdate.img;
+    }
+    loadTableProduct(arrProduct);
+    saveProducts();
+    document.querySelector("#id").disabled = false; 
+}
+
+
+
+// Search san pham
+document.querySelector('#frmSearch').onsubmit = (e) => {
+    e.preventDefault();
+    let keyword = document.querySelector("#keyword").value; 
+    keyword = ToSlug(keyword);
+    let arrProductSearch = arrProduct.filter((prodItem) => ToSlug(prodItem.name).search(keyword) !=-1); 
+    loadTableProduct(arrProductSearch)
+}
+
+window.ToSlug = (str) => {
+  return str
+    .toLowerCase()
+    .normalize('NFD')                    // tách dấu tiếng Việt
+    .replace(/[\u0300-\u036f]/g, '')     // bỏ dấu
+    .replace(/[^a-z0-9\s]/g, '')         // giữ chữ, số, khoảng trắng
+    .trim()
+    .replace(/\s+/g, '-');               // khoảng trắng -> dấu gạch
 }
--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,12 @@
 export class Product { 
     id = 0;
-    name = '';
+    name = "";
     price = 0;
-    img = '';
-    type = '';
+    img = "";
+    type = "";
     
     //constructor: hàm khởi tạo
-    constructor(_id = 0, _name = '', _price = 0, _img = '') {
+    constructor(_id = 0, _name = "", _price = 0, _img = "") {
         this.id = _id;
         this.name = _name;
         this.price = _price;
